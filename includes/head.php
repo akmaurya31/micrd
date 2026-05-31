9bd2a55d72597eb700b6453e69c5de389cec8a78
akmaurya31 <akmaurya31@gmail.com>
2026-05-31 13:46:26 +0530

    add

--- includes/head.php
+++ includes/head.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title : "NBDIUP Portal"; ?></title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="/css1.css">
     <link rel="stylesheet" href="/css.css">
